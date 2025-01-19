4c3151c16206aea598f57fdf42d9920c8c322f39
asiuzzaman <zaman1304006@gmail.com>
2025-01-19 14:15:22 +0600

    Add audio file, app Background

--- HerryPorter Trivia/HerryPorter Trivia/ContentView.swift
+++ HerryPorter Trivia/HerryPorter Trivia/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        GeometryReader { geo in
+            ZStack {
+                Image("hogwarts")
+                    .resizable()
+                    .frame(width: geo.size.width * 3, height: geo.size.height)
+                    .padding(.top, 3)
+
+
+            }.frame(width: geo.size.width, height: geo.size.height
+            )
+
+        }.ignoresSafeArea()
     }
 }
 
