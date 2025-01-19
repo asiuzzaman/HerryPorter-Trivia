e68882bc90de6fb6af0f328caa89c78a2cfa7a81
asiuzzaman <zaman1304006@gmail.com>
2025-01-19 12:46:35 +0600

    First commit

---
+++ HerryPorter Trivia/HerryPorter Trivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HerryPorter Trivia
+//
+//  Created by Md. Asiuzzaman on 19/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HerryPorter Trivia/HerryPorter Trivia/HerryPorter_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerryPorter_TriviaApp.swift
+//  HerryPorter Trivia
+//
+//  Created by Md. Asiuzzaman on 19/1/25.
+//
+
+import SwiftUI
+
+@main
+struct HerryPorter_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
