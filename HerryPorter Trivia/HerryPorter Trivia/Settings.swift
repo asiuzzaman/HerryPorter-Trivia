83761a14e35d876dbfd2085b6f906a12c1042da5
asiuzzaman <zaman1304006@gmail.com>
2025-01-30 01:56:37 +0600

    Design settings screen

---
+++ HerryPorter Trivia/HerryPorter Trivia/Settings.swift
@@ -0,0 +1,74 @@
+//
+//  Settings.swift
+//  HerryPorter Trivia
+//
+//  Created by Md. Asiuzzaman on 30/1/25.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    @Environment(\.dismiss) private var dismiss
+    var body: some View {
+        ZStack {
+            InfoBackgroundImage()
+            VStack {
+                Text("Which book would you like to see question from?")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(), GridItem()]) {
+                        ZStack(alignment: .bottomTrailing) {
+                            Image("hp1")
+                                .resizable()
+                                .scaledToFit()
+                                .shadow(radius: 7)
+                            Image(systemName: "checkmark.circle.fill")
+                                .font(.largeTitle)
+                                .imageScale(.large)
+                                .foregroundColor(.green)
+                                .shadow(radius: 1)
+                                .padding(3)
+                        }
+
+                        ZStack(alignment: .bottomTrailing) {
+                            Image("hp2")
+                                .resizable()
+                                .scaledToFit()
+                                .shadow(radius: 7)
+                                .overlay(Rectangle().opacity(0.33))
+                            Image(systemName: "circle")
+                                .font(.largeTitle)
+                                .imageScale(.large)
+                                .foregroundColor(.green.opacity(0.5))
+                                .shadow(radius: 1)
+                                .padding(3)
+                        }
+
+                        ZStack {
+                            Image("hp3")
+                                .resizable()
+                                .scaledToFit()
+                                .shadow(radius: 7)
+                                .overlay(Rectangle().opacity(0.75))
+                            Image(systemName: "lock.fill")
+                                .font(.largeTitle)
+                                .imageScale(.large)
+                                .shadow(color: .white.opacity(0.75), radius: 3)
+                                .padding(3)
+                        }
+                    }
+                    .padding()
+                }
+
+                Button ("Done") {
+                    dismiss()
+                }.doneButton()
+            }
+        }
+    }
+}
+
+#Preview {
+    Settings()
+}
